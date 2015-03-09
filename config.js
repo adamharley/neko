97e321c25cd46e7aea04d151ca2ed921599bfa55
Adam Harley <adam@adamharley.co.uk>
2015-03-09 17:52:58 +0000

    new options UI, better config loading

--- config.js
+++ config.js
@@ -1,41 +1,27 @@
-chrome.storage.sync.get(['type','startNekoX','startNekoY'], function(items) {
-	var type = 'white';
-	var startNekoX = 0;
-	var startNekoY = 0;
-
-	if (items) {
-		if (items.type) {
-			type = items.type;
+chrome.storage.sync.get(
+	{"type": "white", "startNekoX": 0, "startNekoY": 0},
+	function(options) {
+		if (options.startNekoX < 0) {
+			options.startNekoX += window.innerWidth;
 		}
-		if (items.startNekoX) {
-			startNekoX = items.startNekoX;
-			
-			if (startNekoX < 0) {
-				startNekoX += window.innerWidth;
-			}
+	
+		if (options.startNekoY < 0) {
+			options.startNekoY += window.innerHeight;
 		}
-		if (items.startNekoY) {
-			startNekoY = items.startNekoY;
-			
-			if (startNekoY < 0) {
-				startNekoY += window.innerHeight;
-			}
-		}
-	}
 
-	var config =
-	"remoteimages='"+chrome.extension.getURL('/images/')+"';\
-	NekoType='"+type+"';\
-	startNekoX="+startNekoX+";\
-	startNekoY="+startNekoY+";";
+		var config =
+		"remoteimages='"+chrome.extension.getURL('/images/')+"';\
+		NekoType='"+options.type+"';\
+		startNekoX="+options.startNekoX+";\
+		startNekoY="+options.startNekoY+";";
 
-	// Load config
-	var script = document.createElement('script');
-	script.appendChild(document.createTextNode(config));
-	(document.body || document.head || document.documentElement).appendChild(script);
+		// Load config
+		var script = document.createElement('script');
+		script.appendChild(document.createTextNode(config));
+		(document.body || document.head || document.documentElement).appendChild(script);
 
-	// Load neko.js
-	script = document.createElement('script');
-	script.setAttribute('src',chrome.extension.getURL('neko.js'));
-	(document.body || document.head || document.documentElement).appendChild(script);
+		// Load neko.js
+		script = document.createElement('script');
+		script.setAttribute('src',chrome.extension.getURL('neko.js'));
+		(document.body || document.head || document.documentElement).appendChild(script);
 });
